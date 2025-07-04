0fc8aa9d38c20c4c8a3ed9681e72acfc2b5b0b5b
FedeGioia <68955237+FedeGioia@users.noreply.github.com>
2025-07-04 02:54:17 -0300

    a

--- restaurant-backend/models/Usuario.js
+++ restaurant-backend/models/Usuario.js
@@ -7,6 +7,7 @@
     password: { type: DataTypes.STRING, allowNull: false },
     rol_id: { type: DataTypes.INTEGER, defaultValue: 2 },
     eliminado: { type: DataTypes.TINYINT, defaultValue: 0 },
+    eliminado_por: { type: DataTypes.STRING, allowNull: true },
   }, {
     timestamps: true,
     createdAt: 'fecha_creacion',
